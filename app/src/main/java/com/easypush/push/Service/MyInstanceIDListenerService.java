82f523010c07c3222a75ed90698a7731b1fe86c7
apique <mailbox.candra@gmail.com>
2016-02-16 11:22:05 +0700

    ‘’

---
+++ app/src/main/java/com/easypush/push/Service/MyInstanceIDListenerService.java
@@ -0,0 +1,7 @@
+package com.easypush.push.Service;
+
+/**
+ * Created by algo on 2/16/16.
+ */
+public class MyInstanceIDListenerService extends com.easypush.easypushlib.EasypushInstanceIDListenerService {
+}
